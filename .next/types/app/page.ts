7ae799c0a44ec562e4fa4dafa9eec8448452cc50
Augusto Henrique Engler <augustoengler.dev@gmail.com>
2026-01-29 17:19:28 -0300

    mds

--- .next/types/app/page.ts
+++ .next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: C:\Users\augus\Documents\GitHub\Liberty-Operations-HUB\app\page.tsx
+// File: C:\Users\LIBERTY\Documents\GitHub\Liberty-Operations-HUB\app\page.tsx
 import * as entry from '../../../app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
